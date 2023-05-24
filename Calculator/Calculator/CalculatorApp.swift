a52a14c4e2232b18f8837c56a0e614b07d883dea
etudiant <Benjamin.valleix@gmail.com>
2023-05-24 09:19:59 +0200

    :zap: Initial commit

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by etudiant on 24/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
