3203ae6a3dac881fd8cdc1054d9f5613d92c0f2f
etudiant <Benjamin.valleix@gmail.com>
2023-05-26 23:10:49 +0200

    :zap: Ajout Capsule pour note

--- Calculator/Calculator/View/ItemView.swift
+++ Calculator/Calculator/View/ItemView.swift
@@ -1,29 +1,41 @@
 import SwiftUI
 
 struct ItemView: View {
+    
+    @State private var note: Double = 20.0
+    
     var body: some View {
         NavigationStack{
-            HStack{
-                VStack{
-                    HStack{
-                        Text("UE1 - Génie logiciel")
-                        Spacer()
-                        Text("6")
-                    }
-                    .padding(.horizontal)
-                    HStack{
-                        Rectangle()
-                            .frame(width: 100, height: 30)
-                            .cornerRadius(20)
-                            .foregroundColor(Color("ColorOK"))
-                        Text("12.22")
-                        Spacer()
-                    }
-                    .padding(.horizontal)
-                    Divider()
+            VStack{
+                HStack{
+                    Text("UE1 - Génie logiciel")
+                    Spacer()
+                    Text("6")
                 }
+                .padding(.horizontal)
+                HStack{
+                    GeometryReader { geometry in
+                        HStack(spacing: 0) {
+                            Capsule()
+                                .frame(width: max(CGFloat(note / 20) * 200, 20), height: 30)
+                                .foregroundColor(note < 10 ? .red : .green)
+                                .gesture(DragGesture()
+                                    .onChanged { value in
+                                        let width = max(min(value.location.x, geometry.size.width), 0)
+                                        let percentage = width / geometry.size.width
+                                        note = percentage * 20
+                                    }
+                                )
+                            Text("\(note, specifier: "%.1f")")
+                                .padding(.horizontal)
+                        }
+                    }
+                    Spacer()
+                }
+                .padding(.horizontal)
+                .padding(.bottom)
+                Divider()
             }
-            
         }
     }
 }
