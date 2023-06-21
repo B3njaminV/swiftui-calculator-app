cfcc56cdcd850dc87016062b9fb58899fbad45a6
etudiant <Benjamin.valleix@gmail.com>
2023-06-21 23:27:04 +0200

    :zap: Update stub

--- Stub/Sources/Stub/Stub.swift
+++ Stub/Sources/Stub/Stub.swift
@@ -10,7 +10,7 @@ public struct Stub : DataManager {
         
         let bloc1 = Bloc(withName: "Total", andUes: [
             UE(withNumero:1, andName: "Génie logiciel", andCoefficient: 6, andMatiere: [
-                Matiere(withName:  "Processus de développement", andCoefficient: 4, andNote: 19.04),
+                Matiere(withName:  "Processus de développement", andCoefficient: 4, andNote: 10.00),
                 Matiere(withName:  "Programmation objets", andCoefficient: 9, andNote: 10.00),
                 Matiere(withName:  "Qualité de développement", andCoefficient: 5, andNote: 10.00),
                 Matiere(withName:  "Remise à niveau Objets", andCoefficient: 4, andNote: 10.00),
@@ -20,15 +20,35 @@ public struct Stub : DataManager {
                 Matiere(withName: "Réseaux", andCoefficient: 4, andNote: 10.00),
                 Matiere(withName: "Services Mobiles", andCoefficient: 4, andNote: 10.00),
                 Matiere(withName: "Système", andCoefficient: 5, andNote: 10.00)
+            ]),
+            UE(withNumero: 3, andName: "Insertion Professionnelle", andCoefficient: 6, andMatiere: [
+                Matiere(withName: "Anglais", andCoefficient: 5, andNote: 10.00),
+                Matiere(withName: "Economie", andCoefficient: 4, andNote: 10.00),
+                Matiere(withName: "Gestion", andCoefficient: 3, andNote: 10.00),
+                Matiere(withName: "Communication", andCoefficient: 4, andNote: 10.00)
+            ]),
+            UE(withNumero: 4, andName: "Technologies Mobiles 1", andCoefficient: 9, andMatiere: [
+                Matiere(withName: "Android", andCoefficient: 6, andNote: 10.00),
+                Matiere(withName: "Architecture de projets C# .NET (1)", andCoefficient: 5, andNote: 10.00),
+                Matiere(withName: "C++", andCoefficient: 4, andNote: 10.00),
+                Matiere(withName: "Swift", andCoefficient: 5, andNote: 10.00)
+            ]),
+            UE(withNumero: 5, andName: "Technologies Mobiles 2", andCoefficient: 9, andMatiere: [
+                Matiere(withName: "Architecture de projets C# .NET (2)", andCoefficient: 4, andNote: 10.00),
+                Matiere(withName: "Client/Serveur", andCoefficient: 4, andNote: 10.00),
+                Matiere(withName: "iOS", andCoefficient: 5, andNote: 10.00),
+                Matiere(withName: "Multiplateformes", andCoefficient: 3, andNote: 10.00),
+                Matiere(withName: "QT Quick", andCoefficient: 5, andNote: 10.00),
+                Matiere(withName: "Xamarin", andCoefficient: 5, andNote: 10.00)
             ])
         ])
         
         let bloc2 = Bloc(withName: "Projet / Stage", andUes: [
-            UE(withNumero:1, andName: "Projet", andCoefficient: 6, andMatiere: [
-                Matiere(withName:  "Projet", andCoefficient: 4, andNote: 10.00)
+            UE(withNumero:6, andName: "Projet", andCoefficient: 9, andMatiere: [
+                Matiere(withName:  "Projet", andCoefficient: 1, andNote: 10.00)
             ]),
-            UE(withNumero: 2, andName: "Stage", andCoefficient: 6, andMatiere: [
-                Matiere(withName: "Stage", andCoefficient: 4, andNote: 10.00)
+            UE(withNumero: 7, andName: "Stage", andCoefficient: 15, andMatiere: [
+                Matiere(withName: "Stage", andCoefficient: 1, andNote: 10.00)
             ])
         ])
         
